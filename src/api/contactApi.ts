187810478044c3322163c231cc77127fadcc33cd
jamesvweston <james@approveme.com>
2019-10-23 13:21:42 -0400

    Removing formatted code

--- src/api/contactApi.ts
+++ src/api/contactApi.ts
@@ -9,14 +9,5 @@ export const contactApi = {
 			method: 'GET',
 			params,
 		})
-			.then((res) => res.data)
-			.then((data) =>
-				data.map((c) => ({
-					firstName: c.first_name,
-					lastName: c.last_name,
-					email: c.email_addresses[0] && c.email_addresses[0].value,
-					imageUrl: c.image_url,
-				}))
-			)
 	},
 }
