77b15cb585ee95191ebcfee9b2fd3fbbf61c56c0
Nazariy995 <nazariy1995@gmail.com>
2019-12-04 11:36:33 -0500

    ESLINT fixes

--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -161,15 +161,15 @@ export const auth = (state = initialState, action) => {
 			})
 		case authActions.UPDATE_AUTH_PHOTO_FULFILLED:
 			return state.merge({
-                updatePhoto: stateUtility.getObserverFulfilled(),
-                user: {
-                    ...state.toJS().user,
-                    data: {
-                        ...state.toJS().user.data,
-                        image_url: state.toJS().user.data.image_url + '?date=' + Math.floor(Date.now())
-                    }
-                }
-            });
+				updatePhoto: stateUtility.getObserverFulfilled(),
+				user: {
+					...state.toJS().user,
+					data: {
+						...state.toJS().user.data,
+						image_url: state.toJS().user.data.image_url + '?date=' + Math.floor(Date.now()),
+					},
+				},
+			})
 		case authActions.UPDATE_AUTH_PHOTO_REJECTED:
 			return state.merge({
 				updatePhoto: stateUtility.getObserverRejected(action.payload),
